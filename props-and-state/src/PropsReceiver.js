b0fa566298c02844383537c8890bd283c5311d34
Jacob Avlund <jacob@avlund.dk>
2018-04-04 10:32:44 +0200

    Tiny additional detail

--- props-and-state/src/PropsReceiver.js
+++ props-and-state/src/PropsReceiver.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const PropsReceiver = props => {
   return (
-    <p>
-      Modtaget som props: {props.amount}
-    </p>
+    <div>
+      <p>
+        Modtaget som props: {props.amount}
+      </p>
+      {
+        props.amount % 10 === 0 && 
+        <p>Tallet går op i 10!</p>
+      }
+    </div>
+    
   );
 };
 
